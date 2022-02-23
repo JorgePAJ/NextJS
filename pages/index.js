49f8ce322896f6bfb320918679f83b6676bd27a8
Jorge Plasencia <66725751+WaberHoruhe@users.noreply.github.com>
2022-02-23 16:10:18 -0600

    first commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Container from "../components/container";
+import Head from "next/head";
+
+const Index = () => {
+  return (
+    <Container>
+      <Head>
+        <title>Next.js Project - Home</title>
+      </Head>
+      <h1>Index</h1>
+    </Container>
+  );
+};
+
+export default Index;
